9b1ec71c0334f8eb74c56d3912a46b1b1a1bae8a
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-02-11 14:30:44 +0100

    Prima versione

---
+++ src/main/java/it/sirfin/scuolajpa/ScuolajpaApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.scuolajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScuolajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScuolajpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/sirfin/scuolajpa/ScuolajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.scuolajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuolajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
